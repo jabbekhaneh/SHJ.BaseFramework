74a2ee2b88758a948906f2e02c86a2f5cda275f9
unknown <jabbekhaneh@gmail.com>
2023-12-12 12:03:27 -0800

    BigFixed Options

--- src/SHJ.BaseFramework.Shared/BaseOptions.cs
+++ src/SHJ.BaseFramework.Shared/BaseOptions.cs
@@ -10,24 +10,50 @@ public class BaseOptions
     /// The amount of DatabaseType should be equal to DbTest
     /// </summary>
     public string DefualtConnectionString { get; set; } = string.Empty;
+    /// <summary>
+    /// 
+    /// </summary>
     public string DatabaseName { get; set; } = string.Empty;
+    /// <summary>
+    /// 
+    /// </summary>
     public string SchemaName { get; set; } = "dbo";
+    /// <summary>
+    /// 
+    /// </summary>
     public string DataSource { get; set; } = ".";
+    /// <summary>
+    /// 
+    /// </summary>
     public string IntegratedSecurity { get; set; } = "True";
+    /// <summary>
+    /// 
+    /// </summary>
     public string MultipleActiveResultSets { get; set; } = "True";
+    /// <summary>
+    /// 
+    /// </summary>
     public DatabaseType DatabaseType { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
     public string UserID { get; set; } = string.Empty;
+    /// <summary>
+    /// 
+    /// </summary>
     public string Password { get; set; } = string.Empty;
+    /// <summary>
+    /// 
+    /// </summary>
     public string InMemoryDatabaseConnection { get; private set; } = @"Data Source=app.db";
 
 
     private string GetConnectionString()
     {
         if (string.IsNullOrEmpty(UserID))
-             return $"data source ={DataSource}; initial catalog ={DatabaseName}; integrated security = {IntegratedSecurity}; MultipleActiveResultSets={MultipleActiveResultSets}";
+             return $"data source ={DataSource};initial catalog={DatabaseName};integrated security={IntegratedSecurity}; MultipleActiveResultSets={MultipleActiveResultSets}";
         else
-            return  $@"Data Source={DataSource} ;Initial Catalog={DatabaseName};Persist Security Info=True;MultipleActiveResultSets=True;User ID={UserID};Password={Password}";
-        
+            return  $@"Data Source={DataSource};Initial Catalog={DatabaseName};Persist Security Info=True;MultipleActiveResultSets=True;User ID={UserID};Password={Password}";
     }
 }
 public enum DatabaseType : byte
