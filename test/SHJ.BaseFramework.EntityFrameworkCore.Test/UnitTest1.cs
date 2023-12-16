66aa16add80f88f9d256dd7829ba4e6d63adafb4
unknown <jabbekhaneh@gmail.com>
2023-12-16 05:06:02 -0800

    BuxFix repository refrence

---
+++ test/SHJ.BaseFramework.EntityFrameworkCore.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace SHJ.BaseFramework.EntityFrameworkCore.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+
+        }
+    }
+}
